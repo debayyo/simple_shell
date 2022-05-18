e9d0d8d52d03a55e9b218a113a5fbe54863e788b
debayyo <adegbitequadriadebayo@gmail.com>
2022-05-18 08:42:28 -0700

    shell

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <elf.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
